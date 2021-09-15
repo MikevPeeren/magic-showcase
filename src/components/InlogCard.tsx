37bf5fddc66d519f9e7ce0d80ac8d2936d37fb25
MikevPeeren <MikevPeeren@hotmail.com>
2021-09-15 14:06:14 +0200

    :sparkles: added Submit Button

--- src/components/InlogCard.tsx
+++ src/components/InlogCard.tsx
@@ -1,14 +1,15 @@
 import { ReactElement, FC } from "react";
 
 import Input from "./Input";
+import SubmitButton from "./SubmitButton";
 
 const InlogCard: FC = (): ReactElement => {
   return (
-    <div className="flex items-center justify-center bg-white w-1/4 h-2/4 rounded-xl p-6">
-      <div className="w-3/4 text-center">
+    <div className="bg-white w-1/4 h-2/4 rounded-xl p-6">
+      <div className="w-full h-full text-center flex flex-col justify-center items-center">
         <div>TODO: Image based for login</div>
         <Input title={"email"} inputID={"email"} inputName={"email"} />
-        <div>TODO: Submit button</div>
+        <SubmitButton />
       </div>
     </div>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ import { ReactElement, FC } from "react";
 const Input: FC = ({ title, inputID, inputName }): ReactElement => {
   return (
     <input
-      className="w-full rounded-xl border p-2"
+      className="w-3/4 rounded-xl border p-2 mt-6"
       id={inputID}
       name={inputName}
       title={title}
