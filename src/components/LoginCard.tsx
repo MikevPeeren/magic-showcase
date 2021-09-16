e3a3097d2ede1eed63da75d9b97eef55e2c7adce
MikevPeeren <MikevPeeren@hotmail.com>
2021-09-16 15:36:49 +0200

    :recycle: changing height

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -7,7 +7,7 @@ import SubmitButton from "./SubmitButton";
 // w-5/6 h-5/6 md:w-2/4 md:h-2/4 lg:w-1/4 sm:w-3/4 sm:h-2/4
 const LoginCard: FC = (): ReactElement => {
   return (
-    <div className="bg-white xl:w-2/6 xl:h-3/4 h-3/4 sm:w-2/4 w-4/5 rounded-2xl">
+    <div className="bg-white xl:w-2/6 xl:h-3/5 h-3/4 sm:w-2/4 w-4/5 rounded-2xl">
       <div className="w-full h-full text-center flex flex-col justify-center items-center">
         <LoginIcon />
         <div className="py-4">
