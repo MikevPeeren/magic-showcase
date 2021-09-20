01d927d96b7348071f26acce5cdb12fde354a3a2
MikevPeeren <MikevPeeren@hotmail.com>
2021-09-20 10:08:24 +0200

    :sparkles: added magic login

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,12 +1,27 @@
-import { ReactElement, FC } from "react";
+import { ReactElement, FC, useEffect, useState } from "react";
 
 import styles from "./SubmitButton.module.scss";
 
-const SubmitButton: FC = (): ReactElement => {
+import { magic } from "../utils/magic";
+
+import { ISubmitButton } from "../../types";
+
+const SubmitButton: FC<ISubmitButton> = ({
+  email,
+}: ISubmitButton): ReactElement => {
+  const handleLogin = async (e) => {
+    const email = "mikevpeeren@hotmail.com";
+    const redirectURI = "http://localhost:3000";
+    if (email) {
+      await magic.auth.loginWithMagicLink({ email, redirectURI });
+    }
+  };
+
   return (
     <button
       className={`${styles.SubmitButton} button rounded-xl text-white h-10 w-2/4 mt-4`}
       type="button"
+      onClick={handleLogin}
     >
       Log In
     </button>
--- types/index.ts
+++ types/index.ts
@@ -2,4 +2,9 @@ export interface IInput {
   title: string;
   inputID: string;
   inputName: string;
+  inputChangedFunction: Function;
+}
+
+export interface ISubmitButton {
+  email: string;
 }
